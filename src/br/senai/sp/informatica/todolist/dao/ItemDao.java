2b58f10d5606ebb4ad2afe5f7024bceb5006daa4
kevinmartiniano <kevin.martiniano123@gmail.com>
2016-09-24 16:31:15 -0300

    Configurações do REST

--- src/br/senai/sp/informatica/todolist/dao/ItemDao.java
+++ src/br/senai/sp/informatica/todolist/dao/ItemDao.java
@@ -4,9 +4,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.senai.sp.informatica.todolist.modelo.itemLista;
 
 @Repository
 public class ItemDao {
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Transactional
+	public void marcarFeito(Long idItem, boolean valor) {
+		itemLista item = manager.find(itemLista.class, idItem);
+		item.setFeito(valor);
+		manager.merge(item);
+	}
 }
